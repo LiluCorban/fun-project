2d06d369c9e5a39a5b78b67d71a08a3c7928b978
Yuliia Hryhorieva <yu.hryhorieva@gmail.com>
2023-04-11 09:27:41 +0300

    check deploy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Main from "./Components/Main/main";
 import Parties from "./Components/Parties/parties";
 import {Route, Routes} from "react-router-dom";
 import How from "./Components/How/How";
-
 import Pricing from "./Components/Pricing/Pricing";
 import Footer from "./Components/Footer/footer";
 
@@ -18,13 +17,8 @@ function App(props) {
       <Routes>
       <Route path="/main" element={<Parties/>}/>
           <Route path="/how-it-works" element={<How/>}/>
-         <Route path="/pricing" element={<Pricing products={props.state.main.section3}/>}/>
-
-
-      </Routes>
+         <Route path="/pricing" element={<Pricing products={props.state.main.section3}/>}/></Routes>
           <Footer links={props.state.header}/>
-
-
 
 </div>
 
